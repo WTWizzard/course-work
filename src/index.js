3207a0fb5c0ebb8bad46bdbfe57ddc80def11f15
Vitalii <faringar35@gmail.com>
2023-05-22 22:34:16 +0300

    change: worked on parsing of api response, and appending it to html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const API_KEY = "93b4145fb1475a05aa9038221f49e696";
 
-const getWeatherData = async (name = "Odessa", countryCode = "UA") => {
+const getWeatherData = async (name = "Odesa", countryCode = "UA") => {
   const cityData = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${name},_,${countryCode}&limit=5&appid=${API_KEY}`
   ).then((res) => res.json());
@@ -18,13 +18,38 @@ const getWeatherData = async (name = "Odessa", countryCode = "UA") => {
 };
 
 
+const search = async () => {
+  const result = await getWeatherData(searchPanel.value, "UA");
+
+    const tempElement = document.querySelector(".main__wather-info-city-temperature");
+    const cityNameElement = document.querySelector(".main__wather-info-city-name");
+    const windSpeedElement = document.querySelector(".main__weather-info-city-wind");
+    const POPElement = document.querySelector(".main__wather-info-city-pop");
+    
+    cityNameElement.innerText = result.city.name;
+    tempElement.innerText = Math.ceil(result.list[0].main.temp);
+    windSpeedElement.innerText = `Wind speed: ${result.list[0].wind.speed} m/s`;
+    POPElement.innerText = result.list[0].pop * 100;
+}
+
 window.onload = async function () {
   const result = await getWeatherData();
   const tempElement = document.querySelector(".main__wather-info-city-temperature");
-  const cityNameElement = document.querySelector(".main__wather-info-city-name")
+  const cityNameElement = document.querySelector(".main__wather-info-city-name");
+
+  const POPIcon = document.createElement("i");
+  POPIcon.classList.add("main__weather-info-city-pop-i");
+
+  const POPElement = document.querySelector(".main__wather-info-city-pop");
+  POPElement.innerText = result.list[0].pop * 100;
+  POPElement.appendChild(POPIcon);
+
+  const windSpeedElement = document.querySelector(".main__weather-info-city-wind");
+  windSpeedElement.innerText = `Wind speed: ${result.list[0].wind.speed} m/s`;
 
   cityNameElement.innerText = result.city.name;
   tempElement.innerText = Math.ceil(result.list[0].main.temp);
+
 }
 
 const searchPanel = document.getElementById("search")
@@ -32,11 +57,11 @@ const searchBtn = document.querySelector('.header__search-icon');
 
 searchBtn.addEventListener("click", async (event) => {
   event.preventDefault()
-  const result = await getWeatherData(searchPanel.value, "IE");
+  await search();
+});
 
-  const tempElement = document.querySelector(".main__wather-info-city-temperature");
-  const cityNameElement = document.querySelector(".main__wather-info-city-name")
-
-  cityNameElement.innerText = result.city.name;
-  tempElement.innerText = Math.ceil(result.list[0].main.temp);
+searchPanel.addEventListener("keypress", async (event) => {
+  if (event.key === "Enter") {
+    await search();
+  }
 })
